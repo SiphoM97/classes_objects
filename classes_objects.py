9fc0d9008fce1c6a77d3f4596724b50337665a05
siphosetu mzayiya <siphosetumzayiya@siphosetus-MacBook-Air.local>
2024-03-13 11:34:19 +0200

    Initial commit

---
+++ classes_objects.py
@@ -0,0 +1,7 @@
+from student import student
+
+student1 = student("Sipho", "Mzayiya", "27", "computer science", "90", True)
+student2 = student("Ellen", "Degeneras", "40", "Journalism", "65", False)
+
+print(student1.surname, student1.major)
+print(student2.is_accepted)
---
+++ student.py
@@ -0,0 +1,11 @@
+class student:
+
+    def __init__(self, name, surname, age, major, average, is_accepted):
+        self.name = name
+        self.surname = surname
+        self.age = age
+        self.major = major
+        self.average = average
+        self.is_accepted = is_accepted
+
+
